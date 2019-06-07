2fab50da74b9eedfe933fd0fa984505fb81ba2fd
dinh dang <dang.dang@episerver.com>
2019-06-07 14:15:25 +0700

    update progressbar for import student

--- Applicaa/Applicaa/FrmImportStudents.Designer.cs
+++ Applicaa/Applicaa/FrmImportStudents.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtLogging = new System.Windows.Forms.TextBox();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblTotalRows = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // progressBar1
@@ -39,6 +41,8 @@
             this.progressBar1.Location = new System.Drawing.Point(23, 66);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(756, 37);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             this.progressBar1.TabIndex = 0;
             // 
             // label1
@@ -50,33 +54,54 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Importing student data";
             // 
-            // textBox1
+            // txtLogging
             // 
-            this.textBox1.Location = new System.Drawing.Point(23, 132);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(756, 255);
-            this.textBox1.TabIndex = 3;
+            this.txtLogging.Location = new System.Drawing.Point(23, 132);
+            this.txtLogging.Multiline = true;
+            this.txtLogging.Name = "txtLogging";
+            this.txtLogging.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogging.Size = new System.Drawing.Size(756, 255);
+            this.txtLogging.TabIndex = 3;
             // 
-            // button1
+            // btnFinish
             // 
-            this.button1.Location = new System.Drawing.Point(666, 403);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 35);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Finish";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnFinish.Location = new System.Drawing.Point(629, 403);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(150, 35);
+            this.btnFinish.TabIndex = 4;
+            this.btnFinish.Text = "Import data";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            this.btnFinish.Click += new System.EventHandler(this.button1_Click);
             // 
-            // ImportStudents
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblTotalRows
+            // 
+            this.lblTotalRows.AutoSize = true;
+            this.lblTotalRows.Location = new System.Drawing.Point(20, 421);
+            this.lblTotalRows.Name = "lblTotalRows";
+            this.lblTotalRows.Size = new System.Drawing.Size(46, 17);
+            this.lblTotalRows.TabIndex = 5;
+            this.lblTotalRows.Text = "label2";
+            // 
+            // FrmImportStudents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 457);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTotalRows);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.txtLogging);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
-            this.Name = "ImportStudents";
+            this.Name = "FrmImportStudents";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admission+";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -87,7 +112,9 @@
 
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLogging;
+        private System.Windows.Forms.Button btnFinish;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblTotalRows;
     }
 }
--- Applicaa/Applicaa/FrmImportStudents.cs
+++ Applicaa/Applicaa/FrmImportStudents.cs
@@ -1,20 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using Applicaa.Helper;
+using Common.DataModel;
 
 namespace Applicaa
 {
     public partial class FrmImportStudents : Form
     {
+
+        int TotalRecords;
         public FrmImportStudents()
         {
             InitializeComponent();
+            lblTotalRows.Text = string.Empty;
+
         }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var obj = new MyWorkerClass();
+            if (!backgroundWorker1.IsBusy)
+            {
+                TotalRecords = GetStudents().Count;
+                progressBar1.Maximum = TotalRecords;
+                backgroundWorker1.RunWorkerAsync(obj);
+                btnFinish.Enabled = false;
+                btnFinish.Text = "Loading...";
+                
+            }
+        }
+
+
+        public List<Student> GetStudents()
+        {
+            var students = new List<Student>();
+            for (int i = 0; i < 335; i++)
+            {
+                students.Add(new Student
+                {
+                    FirstName = "First name ",
+                    LastName = "Last name " + i,
+                    SchoolName = "School name " + i,
+                    Age = 10 + i,
+                    Selected = true
+                });
+            }
+
+            return students;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            var obj = (MyWorkerClass) e.Argument;
+            ProcessData(obj);
+        }
+
+        public void ProcessData(MyWorkerClass obj)
+        {
+            
+            int i = 0;
+            foreach (var student in GetStudents())
+            {
+                obj.Name = student.FirstName + " " + student.LastName;
+                obj.PersonId = student.Age;
+                backgroundWorker1.ReportProgress(i, obj);
+                i++;
+                Thread.Sleep(100);
+            }
+            
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            if (!backgroundWorker1.CancellationPending)
+            {
+                var obj = (MyWorkerClass) e.UserState;
+                txtLogging.AppendText("\n");
+                txtLogging.AppendText(obj.Name + " processed ...");
+                progressBar1.Value = e.ProgressPercentage;
+                lblTotalRows.Text = @"Students processed : " + e.ProgressPercentage + 1;
+
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            lblTotalRows.Text = "All data proceessed !";
+            btnFinish.Enabled = true;
+            btnFinish.Text = "Finish";
+        }
+    }
+
+
+
+    public class MyWorkerClass
+    {
+        public string Name;
+        public int PersonId;
     }
 }
